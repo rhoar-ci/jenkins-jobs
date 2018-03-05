89f014d74b0ec724df27b9d7d50f2604af38012e
Ladislav Thon <lthon@redhat.com>
2018-03-05 13:57:57 +0100

    fix project cleanup for OpenShift Online Starter clusters

--- jobs/scripts/clean-project.sh
+++ jobs/scripts/clean-project.sh
@@ -6,5 +6,7 @@ if [[ "$OPENSHIFT_CLUSTER" =~ "local" ]] && grep -q fake /my-secrets/clusters/$O
   # in development, there's no separate namespace just for running tests
   echo "Actually no, this is development environment"
 else
-  oc delete all --all
+  # `|| true` because OpenShift Online Starter clusters have some strange access restrictions that cause failures;
+  # see https://bugzilla.redhat.com/show_bug.cgi?id=1515703 for more info
+  oc delete all --all || true
 fi
