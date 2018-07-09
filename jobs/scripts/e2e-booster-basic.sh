6611e6d54ad5af94672fd69d40c6de264845e796
Ladislav Thon <lthon@redhat.com>
2018-07-09 17:09:03 +0200

    add end-to-end tests for boosters

---
+++ jobs/scripts/e2e-booster-basic.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+# requires `s2i-functions.sh` to be included before this file
+
+runEnd2EndTest () {
+  local mission=''
+  local runtime=''
+
+  case "$BOOSTER_NAME" in
+    *vertx*) runtime=vertx ;;
+    *wfswarm*) runtime=wfswarm ;;
+    *spring-boot*) runtime=springboot ;;
+  esac
+  case "$BOOSTER_NAME" in
+    *crud*) mission=crud ;;
+    *configmap*) mission=configMap ;;
+    *secured*) mission=securedHttp ;;
+    *health-check*) mission=healthCheck ;;
+    *circuit-breaker*) mission=circuitBreaker ;;
+    *cache*) mission=cache ;;
+    *http*) mission=http ;;
+  esac
+
+  local pomFile='./pom.xml'
+  if [[ -d greeting-service ]] ; then
+    # this is fine for now: it covers all multi-module boosters and doesn't affect single-module boosters
+    pomFile='./greeting-service/pom.xml'
+  fi
+  local routeName=$(xmlstarlet select -N mvn=http://maven.apache.org/POM/4.0.0 --template --value-of '/mvn:project/mvn:artifactId' ${pomFile})
+  local url=$(oc get route ${routeName} -o jsonpath='http://{.spec.host}/')
+
+  echo "Running E2E tests for $runtime/$mission at $url"
+
+#  git clone https://github.com/rhoar-qe/boosters-e2e-tests.git
+  git clone -b stability-improvements https://github.com/Ladicek/rhoar-boosters-e2e-tests.git boosters-e2e-tests
+  pushd boosters-e2e-tests
+  npm install
+  # we only need Chrome, and Gecko driver is hosted at GitHub, which often responds with "API rate limit exceeded"
+  npx webdriver-manager update --gecko=false
+  npm run test -- --suite=${mission} --params.url.${mission}=${url} --params.runtime=${runtime}
+  popd
+}
+
+s2i_setup
+
+runEnd2EndTest
+
+s2i_teardown
---
+++ jobs/scripts/s2i-functions.sh
@@ -0,0 +1,95 @@
+#!/bin/bash
+
+processYamlFile () {
+  local yaml=$1
+  local action=$2
+
+  echo "Processing $yaml"
+
+  local commandSuffix=""
+  if [[ $action == "delete" ]] ; then
+    commandSuffix="|| true" # don't fail everything at the very end
+  fi
+
+  local dir=$(dirname $(dirname $yaml))
+  sed -i -e 's/RUNTIME_VERSION/latest/' $yaml
+  if grep -q 'kind: Template' $yaml ; then
+    # TODO maybe use GIT_BRANCH instead of GIT_COMMIT?
+    eval "oc process -f $yaml -p SOURCE_REPOSITORY_URL=$GIT_URL -p SOURCE_REPOSITORY_REF=$GIT_COMMIT -p SOURCE_REPOSITORY_DIR=$dir | oc $action -f - $commandSuffix"
+  else
+    eval "oc $action -f $yaml $commandSuffix"
+  fi
+}
+
+processYamlFilesOfAKind () {
+  local action=$1
+  local kind=$2
+
+  for yaml in $(find . -type f -regex "^.*/\.openshiftio/$kind\.\(\w+\.\)?ya?ml$") ; do
+    processYamlFile $yaml $action
+  done
+}
+
+emulateLauncherApply () {
+  local action="apply"
+
+  echo "Emulating Launcher: oc $action"
+  processYamlFilesOfAKind $action "resource"
+  processYamlFilesOfAKind $action "service"
+  processYamlFilesOfAKind $action "application"
+}
+
+emulateLauncherDelete () {
+  local action="delete"
+
+  echo "Emulating Launcher: oc $action"
+  processYamlFilesOfAKind $action "application"
+  processYamlFilesOfAKind $action "service"
+  processYamlFilesOfAKind $action "resource"
+}
+
+applyGitWorkaround () {
+  # workaround for https://github.com/openshift/origin/issues/8374
+  cat << EOF > my-gitconfig
+[user]
+        name = Workaround
+        email = workaround@example.com
+EOF
+
+  oc secrets new workaround .gitconfig=my-gitconfig
+  oc annotate secret workaround 'build.openshift.io/source-secret-match-uri-1=*://*/*'
+}
+
+removeGitWorkaround () {
+  # workaround for https://github.com/openshift/origin/issues/8374
+  oc delete secret workaround
+}
+
+applyDeploymentWorkaround () {
+  # OpenShift 3.7 doesn't support Kubernetes deployments all that well,
+  # so we change the JDG YAML file to be a deployment config instead
+  #
+  # when we upgrade to OpenShift 3.9, this should be removed
+  if [[ "$JOB_NAME" =~ "cache" ]] ; then
+    for F in service.cache.yml .openshiftio/service.cache.yml greeting-service/.openshiftio/service.cache.yml ; do
+      if [[ -f $F ]] ; then
+        echo "Editing the JDG resource $F to make it a DeploymentConfig instead of Deployment"
+        sed -i -e "s|apiVersion: extensions/v1beta1|apiVersion: v1|" -e "s|kind: Deployment|kind: DeploymentConfig|" -e "s|replicas: 1|replicas: 1\n      triggers:\n      - type: ConfigChange|" $F
+      fi
+    done
+  fi
+}
+
+s2i_setup () {
+  applyGitWorkaround
+  applyDeploymentWorkaround
+
+  emulateLauncherApply
+  sleep 300 # if only there was a way to actually wait for the build and deployment to finish...
+}
+
+s2i_teardown () {
+  emulateLauncherDelete
+
+  removeGitWorkaround
+}
