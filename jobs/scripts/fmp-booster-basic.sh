ce18a17e538f182466b60f3babab98811800bde3
Ladislav Thon <lthon@redhat.com>
2018-07-18 14:40:27 +0200

    remove the workaround for JDG Deployment now that we moved to OpenShift 3.9

--- jobs/scripts/fmp-booster-basic.sh
+++ jobs/scripts/fmp-booster-basic.sh
@@ -1,17 +1,4 @@
 #!/bin/bash
 
-# OpenShift 3.7 doesn't support Kubernetes deployments all that well,
-# so we change the JDG YAML file to be a deployment config instead
-#
-# when we upgrade to OpenShift 3.9, this should be removed
-if [[ "$JOB_NAME" =~ "cache" ]] ; then
-  for F in service.cache.yml tests/src/test/resources/test-cache.yml tests/src/test/resources/test-cacheserver.yml ; do
-    if [[ -f $F ]] ; then
-      echo "Editing the JDG resource $F to make it a DeploymentConfig instead of Deployment"
-      sed -i -e "s|apiVersion: extensions/v1beta1|apiVersion: v1|" -e "s|kind: Deployment|kind: DeploymentConfig|" -e "s|replicas: 1|replicas: 1\n      triggers:\n      - type: ConfigChange|" $F
-    fi
-  done
-fi
-
 mvn clean verify -B
 mvn clean verify -B -Popenshift,openshift-it
--- jobs/scripts/s2i-functions.sh
+++ jobs/scripts/s2i-functions.sh
@@ -65,24 +65,8 @@ removeGitWorkaround () {
   oc delete secret workaround
 }
 
-applyDeploymentWorkaround () {
-  # OpenShift 3.7 doesn't support Kubernetes deployments all that well,
-  # so we change the JDG YAML file to be a deployment config instead
-  #
-  # when we upgrade to OpenShift 3.9, this should be removed
-  if [[ "$JOB_NAME" =~ "cache" ]] ; then
-    for F in service.cache.yml .openshiftio/service.cache.yml greeting-service/.openshiftio/service.cache.yml ; do
-      if [[ -f $F ]] ; then
-        echo "Editing the JDG resource $F to make it a DeploymentConfig instead of Deployment"
-        sed -i -e "s|apiVersion: extensions/v1beta1|apiVersion: v1|" -e "s|kind: Deployment|kind: DeploymentConfig|" -e "s|replicas: 1|replicas: 1\n      triggers:\n      - type: ConfigChange|" $F
-      fi
-    done
-  fi
-}
-
 s2i_setup () {
   applyGitWorkaround
-  applyDeploymentWorkaround
 
   emulateLauncherApply
   sleep 300 # if only there was a way to actually wait for the build and deployment to finish...
