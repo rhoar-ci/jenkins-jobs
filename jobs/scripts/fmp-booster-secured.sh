e63dfbbc88e290dc1138b8139c189fbc72bbd857
mocenas <mocenas@redhat.com>
2019-10-21 15:30:24 +0200

    add wait for SSO deployment

--- jobs/scripts/fmp-booster-secured.sh
+++ jobs/scripts/fmp-booster-secured.sh
@@ -4,6 +4,11 @@ oc apply -f service.sso.yaml
 
 SSO_URL=$(oc get route secure-sso -o jsonpath='https://{.spec.host}/auth')
 
+while [ $(oc get dc/sso -o jsonpath='{.status.availableReplicas}') -ne 1 ]
+do
+    sleep 10
+done
+
 mvn clean verify -B
 mvn clean verify -B -Popenshift,openshift-it -Dskip.sso.init=true -DSSO_AUTH_SERVER_URL=${SSO_URL}
 
